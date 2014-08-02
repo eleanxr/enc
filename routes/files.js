1c70014336fc21136456a26cc69dbaf1c1b4058d
Will Dicharry <will@linux-tcwl.site>
2014-08-02 15:41:49 -0600

    Basic file upload.

---
+++ routes/files.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+var multiparty = require('multiparty');
+var util = require('util');
+
+router.get('/', function(req, res) {
+  res.send('files will be here');
+});
+
+router.post('/', function (req, res) {
+  var form = new multiparty.Form();
+
+  form.parse(req, function (err, fields, files) {
+    if (err) {
+      res.writeHead(400, {'content-type': 'text/plain'});
+      res.end('invalid request' + err.message);
+      return;
+    }
+    res.writeHead(200, {'content-type': 'text/plain'});
+    res.write('received fields\n\n' + util.inspect(fields));
+    res.write('\n\n');
+    res.end('received files\n\n' + util.inspect(files));
+  });
+});
+
+module.exports = router;
---
+++ test/testupload.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+curl -F "filedata=@data/file.txt" http://localhost:3000/files
